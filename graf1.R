64588a9464193d391c3fb6d49a3d97e2802c3d62
Petr Kočí <petr.koci@ihned.cz>
2013-09-19 21:13:47 +0200

    graf2

--- graf1.R
+++ graf1.R
@@ -1,7 +1,4 @@
-vztahy  <- read.csv("vztahy.csv", sep="|", header=F)
-
-
-write.csv(tabulka[order(-tabulka$Freq),], "tabulka.csv", row.names=F, quote=F)
+vztahy  <- read.csv("vztahy.csv", sep=";", header=F)
 
 
 tabulka  <- as.data.frame(table(vztahy$V10))
@@ -11,23 +8,43 @@ names(tabulka)  <- c("Stát", "Partnerství")
 tabulka  <- tabulka[order(-as.numeric(tabulka$Partnerství)),]
 row.names(tabulka)  <- NULL
 
-text(x,y+2,labels=as.character(y))
+write.csv(tabulka, "tabulka.csv", row.names=F, quote=F)
+
+
 
 y <- as.matrix(tabulka$Partnerství)
 
-colors()
 
-par(mai=c(2.5,1.1,2,0))
+x  <- barplot(tabulka$Partnerství,
+        names.arg=tabulka$Stát,
+        main="Kde leží města, s nimiž česká města navázala partnerství",
+        ylab="Počet uzavřených partnerství",
+        cex.names=0.80,
+        col="orange",
+        border="white",
+        las=2,
+        ylim=c(0,240))
 
+text(x,y+8,labels=as.character(y),cex=0.55)
+
+
+# vytiskni graf
+png("graf1.png",res=600,height=420,width=670)
+par(mar=c(4,4,3,1),
+    omi=c(0.1,0.1,0.1,0.1),
+    mgp=c(3,0.5,0),
+    las=1,
+    mex=0.5,
+    cex.main=0.6,
+    cex.lab=0.5,
+    cex.axis=0.4)
 barplot(tabulka$Partnerství,
         names.arg=tabulka$Stát,
         main="Kde leží města, s nimiž česká města navázala partnerství",
         ylab="Počet uzavřených partnerství",
-        cex.names=0.70,
+        cex.names=0.35,
         col="orange",
         border="white",
         las=2,
-        ylim=c(0,230))
-
-
-text(x,y+5,labels=as.character(y),cex=0.55)
+        ylim=c(0,250))
+dev.off()
---
+++ graf2.R
@@ -0,0 +1,20 @@
+tabulka.roky  <- as.data.frame(table(vztahy$V11))
+
+# očisti tabulku od chyb
+tabulka.roky  <- tabulka.roky[4:53,]
+
+# kolik zbývá záznamů
+sum(tabulka.roky$Freq)
+
+# zformátuj letopočty jako data
+tabulka.roky$d1  <- as.Date(tabulka.roky$Var1, "%Y")
+tabulka.roky$d2  <- strptime(tabulka.roky$Var1, "%Y")
+
+# nakresli graf
+plot(tabulka.roky$Freq~tabulka.roky$d1,
+     type="l",
+     xlab="Rok",
+     ylab="Uzavřená partnerství",
+     main="Kolik partnerství uzavřela česká města v jednotlivých letech",
+     col="orange",
+     lwd=3)

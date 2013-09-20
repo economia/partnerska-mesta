6fea1af3015f88e272156fe4cea578e19d554228
Petr Kočí <petr.koci@ihned.cz>
2013-09-20 14:34:25 +0200

    řazení tabulky vztahů

--- mapa-svet.R
+++ mapa-svet.R
@@ -8,7 +8,7 @@ require(RColorBrewer)
 pocetPartnerstvi  <- as.data.frame(table(vztahy[,4]))
 pocetPartnerstvi  <- pocetPartnerstvi[order(-pocetPartnerstvi$Freq),]
 row.names(pocetPartnerstvi)  <- NULL
-write.csv("pocetpartnerstvi.csv", pocetPartnerstvi)
+write.csv(pocetPartnerstvi, "pocetpartnerstvi.csv")
 
 # našti shapefile s mapou světa
 svetSHP  <- readShapePoly("CNTR_2010_03M_SH/Data/CNTR_RG_03M_2010.shp")
@@ -16,6 +16,15 @@ svetSHP  <- readShapePoly("CNTR_2010_03M_SH/Data/CNTR_RG_03M_2010.shp")
 #barvy
 display.brewer.all()
 paleta  <- brewer.pal(8, "Set1")
+
+#seřaď vztahy podle počtu partnerství
+pp  <- as.numeric(vector())
+for (i in 1:nrow(vztahy)) {
+  pp  <- c(pp, pocetPartnerstvi[pocetPartnerstvi$Var1==vztahy[i, 4], 2])
+}
+vztahy$pp  <- pp
+vztahy  <- vztahy[order(vztahy$pp),]
+
 
 # generuj graf
 png("mapa-svet.png", res=2400, height=14400, width=19200)
@@ -37,32 +46,32 @@ map("world", col="#f2f2f2", fill=T, bg="white", lwd=0.1, xlim=xlim, ylim=ylim)
 for (i in 1:nrow(vztahy)) {
   spojnice <- gcIntermediate(c(vztahy[i,3], vztahy[i,2]), c(vztahy[i,8], vztahy[i,7]), n=50, addStartEnd=TRUE)
   if (vztahy[i,4]=="Brno") {
-    lines(spojnice, col=paleta[1], lwd=0.1)
-  } 
+    lines(spojnice, col=paleta[1], lwd=0.2)
+   } 
   else if (vztahy[i,4]=="Praha")
-  {lines(spojnice, col=paleta[2], lwd=0.1)
+  {lines(spojnice, col=paleta[2], lwd=0.2)
   }
   else if (vztahy[i,4]=="Ostrava")
-  {lines(spojnice, col=paleta[3], lwd=0.1)
+  {lines(spojnice, col=paleta[3], lwd=0.2)
   }
   else if (vztahy[i,4]=="Žamberk")
-  {lines(spojnice, col=paleta[4], lwd=0.1)
+  {lines(spojnice, col=paleta[4], lwd=0.2)
   }
   else if (vztahy[i,4]=="Znojmo")
-  {lines(spojnice, col=paleta[5], lwd=0.1)
+  {lines(spojnice, col=paleta[5], lwd=0.2)
   }
   else if (vztahy[i,4]=="Kutná Hora")
-  {lines(spojnice, col=paleta[6], lwd=0.1)
+  {lines(spojnice, col=paleta[6], lwd=0.2)
   }
   else if (vztahy[i,4]=="Hradec Králové")
-  {lines(spojnice, col=paleta[7], lwd=0.1)
+  {lines(spojnice, col=paleta[7], lwd=0.2)
   }
   else if (vztahy[i,4]=="Bystré")
-  {lines(spojnice, col=paleta[8], lwd=0.1)
+  {lines(spojnice, col=paleta[8], lwd=0.2)
   }
   
   else {
-  lines(spojnice, lwd=0.03)
+  lines(spojnice, lwd=0.1)
   }
 }
 
